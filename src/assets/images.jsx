272be79ccec55530d606f59a8e61a56f39889184
matthewandersonthompson <matthewandersonthompson@gmail.com>
2024-11-09 16:03:17 -0700

    images file, and updated dashboard

---
+++ src/assets/images.jsx
@@ -0,0 +1,48 @@
+// src/assets/images.js
+import ChatBot from './images/ChatBot.png';
+import chatgptScreenshot from './images/chatgpt_screenshot.png';
+import DMTrainingGroundsHomepage from './images/DM_Training_Grounds_Homepage.png';
+import dm from './images/dm.png';
+import dmskills from './images/dmskills.png';
+import Dropdown from './images/Dropdown.png';
+import duolingoQuizScreenshot from './images/duolingo_quiz_screenshot.png';
+import duolingoScreenshot from './images/duolingo_screenshot.png';
+import lesson1 from './images/lesson1.png';
+import lesson2 from './images/lesson2.png';
+import lesson3 from './images/lesson3.png';
+import lesson4 from './images/lesson4.png';
+import lesson5 from './images/lesson5.png';
+import lesson6 from './images/lesson6.png';
+import lesson7 from './images/lesson7.png';
+import lesson8 from './images/lesson8.png';
+import lesson9 from './images/lesson9.png';
+import lesson10 from './images/lesson10.png';
+import Login from './images/Login.png';
+import progressIcon from './images/progress_icon.png';
+import quizLearning from './images/Quiz:Learning.png';
+import test from './images/test.png';
+
+export {
+  ChatBot,
+  chatgptScreenshot,
+  DMTrainingGroundsHomepage,
+  dm,
+  dmskills,
+  Dropdown,
+  duolingoQuizScreenshot,
+  duolingoScreenshot,
+  lesson1,
+  lesson2,
+  lesson3,
+  lesson4,
+  lesson5,
+  lesson6,
+  lesson7,
+  lesson8,
+  lesson9,
+  lesson10,
+  Login,
+  progressIcon,
+  quizLearning,
+  test,
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,96 +2,109 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/app-data.css';
+import {
+  lesson1,
+  lesson2,
+  lesson3,
+  lesson4,
+  lesson5,
+  lesson6,
+  lesson7,
+  lesson8,
+  lesson9,
+  lesson10,
+  test,
+  progressIcon,
+} from '../assets/images';
 
 const Dashboard = () => {
   return (
     <div>
-
       <div className="content">
         <section className="learning-path">
           <h2>DM Training Course</h2>
 
           <div className="lesson">
-            <img src="../assets/images/lesson1.png" alt="Lesson 1 Icon" />
+            <img src={lesson1} alt="Lesson 1 Icon" />
             <Link to="/quizzes">1. Introduction to Being a Dungeon Master</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson2.png" alt="Lesson 2 Icon" />
+            <img src={lesson2} alt="Lesson 2 Icon" />
             <Link to="/quizzes">2. Building a World: Story and Setting</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson3.png" alt="Lesson 3 Icon" />
+            <img src={lesson3} alt="Lesson 3 Icon" />
             <Link to="/quizzes">3. Designing Adventures</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="quiz-modal">
-            <img src="../assets/images/test.png" alt="Quiz Icon" />
+            <img src={test} alt="Quiz Icon" />
             <Link to="/quizzes">Quiz 1: DM Basics</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson4.png" alt="Lesson 4 Icon" />
+            <img src={lesson4} alt="Lesson 4 Icon" />
             <Link to="/quizzes">4. Managing NPCs and Villains</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson5.png" alt="Lesson 5 Icon" />
+            <img src={lesson5} alt="Lesson 5 Icon" />
             <Link to="/quizzes">5. Rules and Mechanics: Understanding D&D Core</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson6.png" alt="Lesson 6 Icon" />
+            <img src={lesson6} alt="Lesson 6 Icon" />
             <Link to="/quizzes">6. Combat Strategies and Encounters</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="quiz-modal">
-            <img src="../assets/images/test.png" alt="Quiz Icon" />
+            <img src={test} alt="Quiz Icon" />
             <Link to="/quizzes">Quiz 2: Storytelling and Combat</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson7.png" alt="Lesson 7 Icon" />
+            <img src={lesson7} alt="Lesson 7 Icon" />
             <Link to="/quizzes">7. Role-Playing: Encouraging Player Engagement</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson8.png" alt="Lesson 8 Icon" />
+            <img src={lesson8} alt="Lesson 8 Icon" />
             <Link to="/quizzes">8. Handling Unexpected Player Actions</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson9.png" alt="Lesson 9 Icon" />
+            <img src={lesson9} alt="Lesson 9 Icon" />
             <Link to="/quizzes">9. Running Epic Boss Fights</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="lesson">
-            <img src="../assets/images/lesson10.png" alt="Lesson 10 Icon" />
+            <img src={lesson10} alt="Lesson 10 Icon" />
             <Link to="/quizzes">10. Campaign Management and Pacing</Link>
           </div>
           <div className="line-between"></div>
 
           <div className="quiz-modal final-exam">
-            <img src="../assets/images/test.png" alt="Final Exam Icon" />
+            <img src={test} alt="Final Exam Icon" />
             <Link to="/quizzes">Final Learning Exam: Professional Dungeon Master</Link>
           </div>
         </section>
 
         <aside className="sidebar">
           <h3>Your Progress</h3>
-          <img src="../assets/images/progress_icon.png" alt="Progress Icon" className="centered-image" />
+          <img src={progressIcon} alt="Progress Icon" className="centered-image" />
           <ul>
             <li>Completed Quizzes: 5</li>
             <li>Chatbot Sessions Completed: 3</li>
@@ -104,7 +117,6 @@ const Dashboard = () => {
           </div>
         </aside>
       </div>
-
     </div>
   );
 };
