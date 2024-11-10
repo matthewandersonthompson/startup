5e39e4b307dd1788349b905f6dab89c0ff314f5b
Matthew Thompson <95981192+matthewandersonthompson@users.noreply.github.com>
2024-11-09 23:39:57 -0700

    cleanup

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/chatbot.css';
-import { dm } from '../assets/images'; // Import the dm image from images.js
+import { dm } from '../assets/images'; 
 
 const Chatbot = () => {
   return (
