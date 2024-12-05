80d38ff612a7df19b94857681ae98ba61396fd86
matthewandersonthompson <matthewandersonthompson@gmail.com>
2024-12-04 21:39:22 -0700

    basic placeholder for authenticated.jsx

--- src/pages/login/authenticated.jsx
+++ src/pages/login/authenticated.jsx
@@ -1,35 +1,17 @@
+// src/pages/login/Authenticated.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import Button from 'react-bootstrap/Button';
+import './login.css';
 
-import './authenticated.css';
-
-export function Authenticated(props) {
+export function Authenticated({ userName, onLogout }) {
   const navigate = useNavigate();
 
-  function logout() {
-    fetch(`/api/auth/logout`, {
-      method: 'delete',
-    })
-      .catch(() => {
-        // Logout failed. Assuming offline
-      })
-      .finally(() => {
-        localStorage.removeItem('userName');
-        props.onLogout();
-      });
-  }
-
   return (
-    <div>
-      <div className='playerName'>{props.userName}</div>
-      <Button variant='primary' onClick={() => navigate('/play')}>
-        Play
-      </Button>
-      <Button variant='secondary' onClick={() => logout()}>
-        Logout
-      </Button>
+    <div className="authenticated-container">
+      <div className="player-name">{userName}</div>
+      <button onClick={() => navigate('/dashboard')}>Go to Dashboard</button>
+      <button onClick={onLogout}>Logout</button>
     </div>
   );
 }
